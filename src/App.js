7967e75999a48de13eed1b73dad8cab0088fc150
edisaurus <edswork.ece@gmail.com>
2021-09-06 07:37:45 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+import Landing from './component/LandingPage';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
 
-export default App;
+  return (
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{ children }</Toast.Body>
+      </Toast>
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <Container className="p-3">
+      <h1 className="center">Welcome To Food-Guru</h1>
+      <ExampleToast>
+        We now have Toasty
+        <span role="img" aria-label="tada">
+          🎉
+        </span>
+      </ExampleToast>
+      <Landing />
+    </Container>
+  );
+};
+
---
+++ src/component/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function LandingPage() {
+  return (
+    <div className="center">
+      <header className="head">Head</header>
+      <main className="main">Main</main>
+      <footer className="footer">Foot</footer>
+    </div>
+  );
+}
